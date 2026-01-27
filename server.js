4f99a6104255dc2cf89d7eabb5cefad86d2fee9c
Kaveesha529 <knbalasooriya529@gmail.com>
2026-01-27 22:20:59 +0530

    added cors

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
 const toDoListRouter = require('./toDoList')
 const app = express()
+
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 app.use(express.json())
 
