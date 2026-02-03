041539ae3d6f0d2cc0033ca6b827e2e0b3c1a39e
Kaveesha529 <knbalasooriya529@gmail.com>
2026-02-03 21:45:16 +0530

    feat(user): enabled cookies

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ require("dotenv").config()
 const cors = require('cors')
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
 const toDoListRouter = require('./toDoList')
 const userRouter = require('./user')
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "http://localhost:5173",
+    credentials: true
 }))
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/user', userRouter)
 app.use('/api/todo-list', toDoListRouter)
